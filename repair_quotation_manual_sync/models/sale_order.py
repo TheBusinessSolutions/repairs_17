aa9c94c5e9bbee815f6d7e9343256df874572c20
alihassanmostafa <alihassan.mostafa@gmail.com>
2026-05-20 23:06:33 +0300

    .

--- repair_quotation_manual_sync/models/sale_order.py
+++ repair_quotation_manual_sync/models/sale_order.py
@@ -1,16 +1,14 @@
-# Copyright 2025 ForgeFlow S.L. (https://www.forgeflow.com)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    needs_repair_sync = fields.Boolean(compute="_compute_needs_repair_sync")
+    needs_repair_sync = fields.Boolean(
+        compute="_compute_needs_repair_sync"
+    )
 
     def _get_states_for_sync(self):
-        """Return the states in which the repair order can be synchronized."""
         return ["draft", "sent"]
 
     @api.depends(
@@ -19,21 +17,34 @@ class SaleOrder(models.Model):
     )
     def _compute_needs_repair_sync(self):
         for order in self:
+            moves = order.repair_order_ids.mapped("move_ids")
             order.needs_repair_sync = any(
-                repair.move_ids.filtered(
+                moves.filtered(
                     lambda m: m.repair_create_sync or m.repair_update_sync
                 )
-                for repair in order.repair_order_ids
             )
 
     def action_sync_repair_lines(self):
-        self.ensure_one()
-        if self.state not in self._get_states_for_sync():
-            return
-        all_moves = self.repair_order_ids.mapped("move_ids")
-        all_moves.filtered("repair_create_sync").with_context(
-            repair_lines_manual_sync=True
-        )._create_repair_sale_order_line()
-        all_moves.filtered("repair_update_sync").with_context(
-            repair_lines_manual_sync=True
-        )._update_repair_sale_order_line()
+        for order in self:
+            if order.state not in order._get_states_for_sync():
+                continue
+
+            moves = order.repair_order_ids.mapped("move_ids")
+
+            create_moves = moves.filtered(
+                lambda m: m.repair_create_sync
+            )
+            if create_moves:
+                create_moves.with_context(
+                    repair_lines_manual_sync=True
+                )._create_repair_sale_order_line()
+
+            update_moves = moves.filtered(
+                lambda m: m.repair_update_sync
+            )
+            if update_moves:
+                update_moves.with_context(
+                    repair_lines_manual_sync=True
+                )._update_repair_sale_order_line()
+
+        return True
--- repair_quotation_manual_sync/models/stock_move.py
+++ repair_quotation_manual_sync/models/stock_move.py
@@ -1,26 +1,24 @@
-# Copyright 2025 ForgeFlow S.L. (https://www.forgeflow.com)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
-
 from odoo import api, fields, models
-from odoo.tools import float_compare, float_is_zero
+from odoo.tools.float_utils import float_compare, float_is_zero
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
 
     repair_invoiceable = fields.Boolean(
-        "Invoiceable",
-        help="Check this box if the product is billable. "
-        "Only invoiceable products will be included in the quotation.",
+        string="Invoiceable",
         default=True,
+        help="Only invoiceable products will be included in the quotation.",
     )
+
     repair_create_sync = fields.Boolean(
         compute="_compute_sync_flags",
     )
+
     repair_update_sync = fields.Boolean(
         compute="_compute_sync_flags",
     )
+
     is_repair_sale_confirmed = fields.Boolean(
         compute="_compute_is_repair_sale_confirmed",
     )
@@ -28,10 +26,9 @@ class StockMove(models.Model):
     @api.depends("repair_id.sale_order_id.state")
     def _compute_is_repair_sale_confirmed(self):
         for move in self:
-            move.is_repair_sale_confirmed = (
-                move.repair_id.sale_order_id.state == "sale"
-                if move.repair_id and move.repair_id.sale_order_id
-                else False
+            move.is_repair_sale_confirmed = bool(
+                move.repair_id.sale_order_id
+                and move.repair_id.sale_order_id.state == "sale"
             )
 
     @api.depends(
@@ -42,45 +39,65 @@ class StockMove(models.Model):
     )
     def _compute_sync_flags(self):
         for move in self:
-            precision = move.product_uom.rounding
-            move.repair_create_sync = (
+            rounding = move.product_uom.rounding
+
+            move.repair_create_sync = bool(
                 move.repair_invoiceable
+                and move.repair_id
+                and move.repair_id.sale_order_id
                 and not move.sale_line_id
                 and not float_is_zero(
-                    move.product_uom_qty, precision_rounding=precision
+                    move.product_uom_qty,
+                    precision_rounding=rounding,
                 )
             )
-            move.repair_update_sync = move.sale_line_id and (
-                float_compare(
-                    move.product_uom_qty,
-                    move.sale_line_id.product_uom_qty,
-                    precision_rounding=precision,
+
+            move.repair_update_sync = bool(
+                move.sale_line_id
+                and (
+                    float_compare(
+                        move.product_uom_qty,
+                        move.sale_line_id.product_uom_qty,
+                        precision_rounding=rounding,
+                    ) != 0
+                    or not move.repair_invoiceable
                 )
-                != 0
-                or not move.repair_invoiceable
             )
 
     def _create_repair_sale_order_line(self):
-        if not self.env.context.get("repair_lines_manual_sync", False):
+        moves = self.filtered(
+            lambda m:
+            m.repair_create_sync
+            and m.repair_id
+            and m.repair_id.sale_order_id
+        )
+
+        if not moves:
             return True
-        return super(
-            StockMove, self.filtered("repair_create_sync")
-        )._create_repair_sale_order_line()
+
+        return super(StockMove, moves)._create_repair_sale_order_line()
 
     def _update_repair_sale_order_line(self):
-        if not self.env.context.get("repair_lines_manual_sync", False):
+        moves = self.filtered("repair_update_sync")
+
+        if not moves:
             return True
-        super(
-            StockMove, self.filtered("repair_update_sync")
-        )._update_repair_sale_order_line()
-        moves_so_line_unlink = self.filtered(
-            lambda m: m.sale_line_id
+
+        super(StockMove, moves)._update_repair_sale_order_line()
+
+        unlink_moves = moves.filtered(
+            lambda m:
+            m.sale_line_id
             and (
                 not m.repair_invoiceable
                 or float_is_zero(
-                    m.product_uom_qty, precision_rounding=m.product_uom.rounding
+                    m.product_uom_qty,
+                    precision_rounding=m.product_uom.rounding,
                 )
             )
         )
-        moves_so_line_unlink._clean_repair_sale_order_line()
+
+        if unlink_moves:
+            unlink_moves._clean_repair_sale_order_line()
+
         return True
